54048db88df6db8804b5bf9b2184e9a35e871a23
Harrish P T <harrishpt@gmail.com>
2023-07-11 23:12:28 +0530

    Leet Code Adding Two numbers target value

---
+++ Leet code.py
@@ -0,0 +1,14 @@
+# Adding Two numbers
+# from typing import List
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 sum = nums[i] + nums[j]
+#                 if sum == target:
+#                     return [i, j]
+#
+#
+# n = Solution()
+# nums = [2, 7, 11, 15]
+# print(n.twoSum(nums, 9))
