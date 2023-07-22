57520f4a0cddae4f3946e0fe45b879f4e70200f5
Harrish P T <harrishpt@gmail.com>
2023-07-23 00:06:53 +0530

    largest element in the array

--- gfg.py
+++ gfg.py
@@ -13,3 +13,21 @@
 # arr = [12, 3, 4, 15]
 # ans = sum(arr)
 # print('Sum of the array is ', ans)
+
+# largest element in the array
+# def largest(arr):
+#     max_value=arr[0]
+#     for i in range(len(arr)):
+#         if arr[i]>max_value:
+#             max_value=arr[i]
+#     return max_value
+# arr=[]
+# arr=[int(x) for x in input("enter the number with space: ").split()]
+# print(largest(arr))
+# or
+# def largest(arr):
+#     ans=max(arr)
+#     return ans
+# arr=[]
+# arr=[int(x) for x in input("enter the number with space: ").split()]
+# print(largest(arr))
