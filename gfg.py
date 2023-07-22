26bb2bc53273cece5833827527bb205100b24e2b
Harrish P T <harrishpt@gmail.com>
2023-07-22 23:50:49 +0530

    # Array #1) Sum of array

---
+++ gfg.py
@@ -0,0 +1,10 @@
+# Find Sum of Array
+# def sum_of_array(arr):
+#     sum=0
+#     for i in arr:
+#         sum+=i
+#     return sum
+#
+# arr=[]
+# arr=[int(x) for x in input("enter the number with space: ").split()]
+# print(sum_of_array(arr))
