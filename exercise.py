18ae8efb2d111e6739f2e7525af2eed96a04747a
PTHARRISH <harrishpt@gmail.com>
2023-08-04 01:06:39 +0530

    exercise error occured

---
+++ exercise.py
@@ -0,0 +1,15 @@
+
+#1. Some Generative Art
+# import sys
+# import random
+# chars="\|/"
+# def draw(rows, columns):
+#     for r in rows:
+#         print(''.join(random.choice(chars) for _ in range(columns)))
+
+# draw(10,20)
+
+
+    
+
+
