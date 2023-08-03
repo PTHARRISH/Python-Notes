43d9d7ab03eb061cd47b63874743a31ede03545c
PTHARRISH <harrishpt@gmail.com>
2023-08-04 00:33:24 +0530

    advance and exercise file added

---
+++ advance.py
@@ -0,0 +1,21 @@
+# comments
+# This is Comment comments are denoted by '#'
+# height=442 #meters(extend to end of the line)
+
+# if-else: If statement is
+
+# looping
+# while loop: while statement loops on condition.
+# while else must print at last of the output if break occurs only not print else statement
+# count=3
+# while count>0:
+#     print('T-minus',count)
+#     count-=1
+# else:
+#     print('Boom!')
+
+# for loop
+
+# nums=[1,7,10,23]
+# for x in nums:
+#     print(x)
