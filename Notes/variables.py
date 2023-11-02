b5bde24319db151443be314d5b983b1e1a85f375
PTHARRISH <harrishpt@gmail.com>
2023-11-03 00:28:20 +0530

    variables and datatypes

---
+++ Notes/variables.py
@@ -0,0 +1,34 @@
+# Variables
+# Variable is containers that store values.
+
+# Python is not “statically typed” it is Dynamically typed :
+#   It doesn’t know about the type of the variable until the code is run.
+#   So declaration is of no use. What it does is,
+#   It stores that value at some memory location and then binds that variable name to that memory container.
+#   And makes the contents of the container accessible through that variable name. 
+#   So the data type does not matter. As it will get to know the type of the value at run-time.
+
+
+# Example 1: 
+Var = "Harrish P.T"
+print(Var)
+
+
+# Example of Variable in Python
+#    * An Example of a Variable in Python is a representational name that serves as a pointer to an object. 
+# Once an object is assigned to a variable, it can be referred to by that name. 
+# In layman’s terms, we can say that Variable in Python is containers that store values.
+
+
+# Rules for Python variables
+#   * A Python variable name must start with a letter or the underscore character.
+#   * A Python variable name cannot start with a number.
+#   * A Python variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+#   * Variable in Python names are case-sensitive (name, Name, and NAME are three different variables).
+#   * The reserved words(keywords) in Python cannot be used to name the variable in Python.
+
+
+# Notes:
+#   The value stored in a variable can be changed during program execution.
+#   A Variables in Python is only a name given to a memory location, 
+# all the operations done on the variable effects that memory location.
