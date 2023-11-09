578ef8d0178db89f0e4bab67128b9c8a32e5cf45
PTHARRISH <harrishpt@gmail.com>
2023-11-09 19:14:23 +0530

    second largest element without sorting

--- codes/print_2ndlargest.py
+++ codes/print_2ndlargest.py
@@ -7,4 +7,11 @@ def print2largest(arr, n):
 def print2largest(arr, n):
 	list2=[x for x in arr if x<max(arr)]
 	return max(list2)
-	
+
+# second largest element without sorting function	
+def secondlargest(arr,n):
+	a=max(arr)
+	arr.remove(a)
+	print(max(arr))
+
+secondlargest([1,0,8,92,100],5)
