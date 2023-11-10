bf6e10913c238187abfb5988ed8bd7b2ead883c1
PTHARRISH <harrishpt@gmail.com>
2023-11-10 22:47:39 +0530

    interchange first and last using swaping

--- geek_for_geeks/Basic/interchange_first_last.py
+++ geek_for_geeks/Basic/interchange_first_last.py
@@ -1,11 +1,33 @@
+# interchange first and last using slicing
 def interchange(arr):
     arr[0],arr[-1]=arr[-1],arr[0]
     return arr
  
-
-
-
 # arr=[int(i) for i in input().split() ]
 arr=list(map(int,input().split(' ')))
 print(interchange(arr))
 
+# interchange first and last using unpacking
+def interchange(arr):
+    get=arr[0],arr[-1]
+    arr[-1],arr[0]=get
+    return arr
+
+arr=list(map(int,input().split(' ')))
+print(interchange(arr))
+
+
+# interchange first and last using swaping
+def interchange(arr):
+    temp=arr[0]
+    arr[0]=arr[-1]
+    arr[-1]=temp
+    return arr
+
+arr=list(map(int,input().split(' ')))
+print(interchange(arr))
+# temp=arr[0]
+    # arr[0]=arr[-1]
+    # arr[-1]=temp
+    # a,*b,c=arr
+    # arr=[c,*b,a]
