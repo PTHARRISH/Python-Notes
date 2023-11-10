fd2672d83f10407470a9a17876f36d556c618391
PTHARRISH <harrishpt@gmail.com>
2023-11-10 22:38:08 +0530

    factorial using in-built function

--- geek_for_geeks/Basic/factorial_3_method.py
+++ geek_for_geeks/Basic/factorial_3_method.py
@@ -3,4 +3,15 @@ def factorial(n):
     return 1 if n==0 or n==1 else n*factorial(n-1)
 
 n=5
-print(f'the factorial of {n} is ',factorial(n),)
+print(f'the factorial of {n} is ',factorial(n))
+# Time Complexity: O(n)
+
+# factorial using in-built function
+import math
+ 
+def factorial(n):
+    return(math.factorial(n))
+ 
+n=5
+print(f'the factorial of {n} is ',factorial(n))
+# Time complexity: O(1)
