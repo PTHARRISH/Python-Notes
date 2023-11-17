7b6b213a903c285aee417b469667748c5e3bafa7
PTHARRISH <harrishpt@gmail.com>
2023-11-17 21:33:35 +0530

    factorial loop method

--- geek_for_geeks/Basic/3.factorial_3_method.py
+++ geek_for_geeks/Basic/3.factorial_3_method.py
@@ -1,4 +1,4 @@
-#factorial using Recursion
+# factorial using Recursion
 def factorial(n):
     return 1 if n==0 or n==1 else n*factorial(n-1)
 
@@ -15,3 +15,14 @@ def factorial(n):
 n=5
 print(f'the factorial of {n} is ',factorial(n))
 # Time complexity: O(1)
+
+# factorial using loop function
+def factorial(n):
+    result=1
+    for i in range(2,n+1):
+        result*=i
+    return result
+
+n=6
+print('The factorial of {0} is {1}'.format(n,factorial(n)))
+# Time Complexity: O(n)
