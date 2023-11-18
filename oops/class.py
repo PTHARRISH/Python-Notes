87baccc2202e1c98954bd7ac7760438cf3acc1d6
PTHARRISH <harrishpt@gmail.com>
2023-11-18 20:15:20 +0530

    class

---
+++ oops/class.py
@@ -0,0 +1,27 @@
+# Introduction to OOPS concept:
+# Python supports both procedure-oriented and object-oriented features.
+# Object-Oriented Programming is a programming paradigm that uses classes and objects to look similar to real life. 
+# OOPs has four central concepts: Encapsulation,Abstraction, Inheritance, and Polymorphism.
+# OOPs mainly works upon two keys is Attributes and Functionality.
+
+# CLASS :
+# A class is a user-defined blueprint or prototype from which objects are created. 
+# Classes provide a means of bundling data and functionality together. 
+# Creating a new class creates a new type of object, allowing new instances of that type to be made. 
+# Each class instance can have attributes attached to it for maintaining its state. 
+# Class instances can also have methods (defined by their class) for modifying their state.
+
+# The class creates a user-defined data structure, which holds its own data members and member functions, 
+# which can be accessed and used by creating an instance of that class. 
+# A class is like a blueprint for an object.
+
+# Some points on Python class:
+# Classes are created by keyword class.
+# Attributes are the variables that belong to a class.
+# Attributes are always public and can be accessed using the dot (.) operator.
+# Classes consist of Data and Functions. 
+# The Data in class is known as Members and functions of the class are known as methods.
+
+
+
+
