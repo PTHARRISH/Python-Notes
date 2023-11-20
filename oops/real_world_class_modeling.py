1d85c0eb62b63db768fb9b14a61c5908d4ad16d1
PTHARRISH <harrishpt@gmail.com>
2023-11-20 10:24:04 +0530

    oops and example

---
+++ oops/real_world_class_modeling.py
@@ -0,0 +1,48 @@
+# Real World Class Modeling
+# Object-oriented programming (OOPs) is a programming paradigm based on objects.
+# Objects that are real-world entities like cars, mobile, trees, etc
+
+# In OOPs, there are generally four pillars that are as follows
+
+# 1. Polymorphism
+# 2. Inheritance
+# 3. Encapsulation
+# 4. Abstraction
+
+# In the following part, you can see how OOPs connect with real-world examples.
+
+# 1. Polymorphism
+# For Polymorphism, let us consider a girl. 
+# The girl can be many things. 
+# She can be Mother, Sister, and Student, etc. 
+# The same person can have different roles.
+# As per the Polymorphism definition in Python, Polymorphism lets us define methods in
+# the child class with the same name as the methods in the parent class.
+
+# 2. Inheritance
+# For Inheritance, let us consider dogs. 
+# The dogs can have the same color, name, size,etc, but they are not the same dog.
+# As per the Inheritance definition in Python, it is a concept in OOPs where a new class 
+# can modify existing classes.
+
+# 3. Encapsulation
+# For Encapsulation, let us consider a company. 
+# A company can have several departments like the Production Department, HR Department, Marketing Department, etc. 
+# All these departments are necessary to make up a company.
+# As per the Encapsulation definition in Python, It is wrapping up variables and methods into a single entity.
+
+# 4. Abstraction
+# For Abstraction, Let us consider a Mobile Phone. 
+# We can do many things on a mobile phone like making a call, taking pictures and playing games, etc. 
+# It doesn’t show the inside process of how it is doing things.
+# As per the Abstraction definition in Python, It is a process of hiding the actual implementation of the method 
+# by only showing a method signature.
+
+
+# Why Real-World Modelling is Needed
+# To make the development and maintenance of projects more effortless.
+# To provide the feature of data hiding that is good for security concerns.
+# We can solve real-world problems if we are using object-oriented programming.
+# It ensures code reusability.
+# It lets us write generic code: which will work with a range of data, 
+# so we don't have to register basic stuff over and over again.
