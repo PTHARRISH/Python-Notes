5a6c2b27be39265de2720f250f98862faa275322
PTHARRISH <harrishpt@gmail.com>
2023-11-22 22:34:10 +0530

    2: Basic Recursion technique

--- codes/8.prime_number.py
+++ codes/8.prime_number.py
@@ -9,3 +9,19 @@ if flag==1:
     print("The {} is not a prime number".format(n))
 else:
     print("The {} is a prime number".format(n))
+
+
+# Method 2: Basic Recursion technique
+num=int(input("enter the number: "))
+def checkPrime(num,iter=2):
+  if num == iter:
+    return True
+  if num%iter==0:
+    return False
+  if num<2:
+    return False
+  return checkPrime(num,iter+1)
+if checkPrime(num)==True:
+   print("The {} is not a prime number".format(num))
+else:
+    print("The {} is a prime number".format(num))
