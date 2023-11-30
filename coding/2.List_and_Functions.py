fc0f341c7ae9f1ae721bd9048191f6d3284f6e64
PTHARRISH <harrishpt@gmail.com>
2023-12-01 00:31:05 +0530

    list exerciese 11

--- coding/2.List_and_Functions.py
+++ coding/2.List_and_Functions.py
@@ -86,40 +86,52 @@ print(my_list)  # Output: [10, 2, 3, 4, 5, 6]
 # Lists are an essential part of Python, and knowing how to use them effectively is crucial 
 # for many programming tasks.
 
-
+print("--------------------------------------------------------------------------")
+print("                              List Exercises                              ")
+print("--------------------------------------------------------------------------")
 # 1.Printing a list
+
+print("1.Printing a list")
 list1=[3,10,5,28,63]
 print(list1)
+print("--------------------------------------------------------------------------")
 
 
 # 2. In Python list can store different types of objects.
 # Create a list containing the given elements: 3, 'spark', True, 0.5, None, 63 and assign to result variable.
 # In response, print this variable to the console
+
+print("2.Store different types of objects in list")
 result=[3,'spark',True,0.5,None,63]
 print(result)
+print("--------------------------------------------------------------------------")
 
 
 # 3.Create an empty list named result. Then, using the list.append() method, 
 # add the following elements to the list: 34,1.5,True,'report.csv' In response, print this list to the console.
 
+print("3.Create and add the following elements to the list")
 result=[]
 result.append(34)
 result.append(1.5)
 result.append(True)
 result.append('report.csv')
 print(result)
+print("--------------------------------------------------------------------------")
 
 
 # 4.Create an empty list named result. Then, using the list.insert() method, 
 # add the following elements to the list (insert each element at the beginning of the list): 
 # 34,1.5,True,'report.csv'.In response, print this list to the console.
 
+print("4.Create and insert each element at the beginning of the list")
 result=[]
 result.insert(0,34)
 result.insert(0,1.5)
 result.insert(0,True)
 result.insert(0,'report.csv')
 print(result)
+print("--------------------------------------------------------------------------")
 
 
 # 5. The following list is given:
@@ -129,6 +141,7 @@ print(result)
 # insert 'aws' at the end of the list
 # In response, print this list to the console.
 
+print("5.Insert 'git' at position with index 1, insert 'aws' at the end of the list")
 techs = ['python', 'django', 'sql', 'html', 'css']
 techs.insert(1,'git')
 techs.append('aws') # append method will insert element at the end of the list
@@ -136,6 +149,7 @@ techs.append('aws') # append method will insert element at the end of the list
 # techs.insert(6,'aws') # you can write its position or you use len function to insert at the last postion
 # techs.insert(len(techs),'aws')
 print(techs)
+print("--------------------------------------------------------------------------")
 
 
 # 6. The following list is given:
@@ -147,11 +161,13 @@ print(techs)
 # css
 # ['python', 'django', 'sql', 'html']
 
+print("6.Pop the last element out and assign it to the result variable")
 techs = ['python', 'django', 'sql', 'html', 'css']
 # print(techs[-1]) # print last element
 result=techs.pop()
 print(result) # to view the last deleted element is assign a variable and print it
 print(techs)
+print("--------------------------------------------------------------------------")
 
 
 # 7. The following list is given:
@@ -163,19 +179,23 @@ print(techs)
 # django
 # ['python', 'sql', 'html', 'css']
 
+print("7.Pop the item with index 1 out from the list and assign it to the result variable")
 techs = ['python', 'django', 'sql', 'html', 'css']
 result=techs.pop(1)
 print(result)
 print(techs)
+print("--------------------------------------------------------------------------")
 
 
 # 8. Using the appropriate method, delete the first encountered element '0101'.
 # In response, print the user_ids variable to the console.
 # Expected result: ['0111', '1030', '0101', '3401', '0111', '1001']
 
+print("8.Delete the first encountered element '0101'")
 user_ids = ['0111', '0101', '1030', '0101', '3401', '0111', '1001']
 user_ids.remove('0101') # use cannot use pop because it is str type it shows error 'str' object cannot be interpreted as an integer
 print(user_ids) # remove method will remove the str on it
+print("--------------------------------------------------------------------------")
 
 
 # 9. The following list is given:
@@ -189,12 +209,14 @@ print(user_ids) # remove method will remove the str on it
 
 # Expected result: User with id '1040' is not in the list.
 
+print("9.Remove a value that is not in the list results in a ValueError")
 user_ids = ['0111', '0101', '1030', '0101', '3401', '0111', '1001']
 user_id = '1040'
 try:
     user_ids.remove(user_id)
 except ValueError:
     print(f"User with id '{user_id}' is not in the list.")
+print("--------------------------------------------------------------------------")
 
 
 # 10. The following list is given:
@@ -202,9 +224,24 @@ except ValueError:
 # Use the del statement to remove penultimate item from the techs list.
 # In response, print the techs list to the console.
 
-
 # Expected output: ['python', 'django', 'sql', 'css']
 
+print("10.Remove penultimate item from the techs list")
 techs=['python', 'django', 'sql', 'html', 'css']
 del techs[-2] # del statement delete the particular element at the position negative index also support
 print(techs) 
+print("--------------------------------------------------------------------------")
+
+
+# 11. The following list is given: techs = ['python', 'django', 'sql', 'html', 'c++', 'git', 'css']
+# Using the del statement remove all items from the techs list except the first and last.
+# In response, print the techs list to the console.
+# Expected result: ['python', 'css']
+
+print("11.Remove all items from the techs list except the first and last")
+techs=['python', 'django', 'sql', 'html', 'css']
+del techs[1:-1] # starting and ending value only displayed
+# del techs[1:6] # del statement delete the particular element at the position of the index
+print(techs)
+print("--------------------------------------------------------------------------")
+
