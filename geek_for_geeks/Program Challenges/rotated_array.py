4825bcad411012eb6d4ad365f7eb29a09f93316a
PTHARRISH <harrishpt@gmail.com>
2023-12-01 23:31:43 +0530

    Rotate the array to the left counter-clockwise

---
+++ geek_for_geeks/Program Challenges/rotated_array.py
@@ -0,0 +1,11 @@
+# Given an unsorted array arr[] of size N. 
+# Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+def rotated_array(arr,D,N):
+    D=D%N #2%5=2 and if 10%5=0 then it will add in D value
+    arr[:]=arr[D:]+arr[:D]
+    return arr
+
+arr=[int(x) for x in input("enter the array numbers: ").split(',') ]
+n=len(arr)
+d=int(input('enter the number: '))
+print(rotated_array(arr,n,d))
