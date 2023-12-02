6ea2967578e96c2e1db0f78721ee4bf565b37a7f
PTHARRISH <harrishpt@gmail.com>
2023-12-02 23:15:06 +0530

    hackerrank weird numbers

---
+++ hackerrank/weird_numbers.py
@@ -0,0 +1,15 @@
+# If else
+#1) weird numbers
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+# n = int(input().strip())
+# if n >= 1 and n <= 100:
+#     if n % 2 != 0 or n > 6 and n <= 20:
+#         print("Weird")
+#     else:
+#         print("Not Weird")
+
+
+
