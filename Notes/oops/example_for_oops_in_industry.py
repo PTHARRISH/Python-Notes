5d90f98c72c011f752615a89f1047b89eea04a5d
PTHARRISH <harrishpt@gmail.com>
2023-12-02 23:15:36 +0530

    oops

---
+++ Notes/oops/example_for_oops_in_industry.py
@@ -0,0 +1,15 @@
+# Example of OOPs using Cars:-
+# Suppose you have details of many cars. 
+# All cars have the same properties like a year of manufacture, Model, Top speed, Color, etc.,
+# and the same functions like start, speed up, speed down, stop, etc. 
+# so we can say that all types of cars have the same information so we can create a common structure for store the information. 
+# This structure is called class and can have different objects. 
+# Each object shows a different type of car with different information.
+
+# Here we will make Car class, and it will work as a basic template for other objects. 
+# We will make car class objects (Ferrari, BMW, and Mercedes). 
+# Each Car Object will have its own, Year of Manufacture, model, Top Speed, color, Engine Power, efficiency, etc.
+
+# The car class would allow the programmer to store similar information unique to each car 
+# (different models, colors, top speeds, etc.) and associate the appropriate information.
+
