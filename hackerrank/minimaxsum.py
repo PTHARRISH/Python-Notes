351333440eb36cc2b7c87119f8e342f175e480ae
PTHARRISH <harrishpt@gmail.com>
2023-12-05 23:22:34 +0530

    Minimum and Maximum in the sum array

---
+++ hackerrank/minimaxsum.py
@@ -0,0 +1,14 @@
+# Minimum and Maximum in the sum array
+def miniMaxSum(arr):
+    # Write your code here
+    news1=0
+    news2=0
+    arr.sort()
+    for i in range(len(arr)-1):
+        news1+=arr[i]
+    for j in range(1,len(arr)):
+        news2+=arr[j]
+    print(news1,news2)
+        
+a=list(map(int,input().split())) # 1 2 3 4 5 6 7
+miniMaxSum(a) # 21 27
