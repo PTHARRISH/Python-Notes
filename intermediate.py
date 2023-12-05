3bebc579d9775315c085ede12706e3f4627cdfdf
PTHARRISH <harrishpt@gmail.com>
2023-12-05 23:36:28 +0530

    delete the code

--- intermediate.py
+++ intermediate.py
@@ -189,9 +189,3 @@
 # force = (g*(m1 * m2))/r*r
 # print(str(force) + "N")
 
-# Get a length in mm and convert to m and km
-mm=float(input('enter the length in mm: '))
-m=mm*0.001
-km=mm*0.000001
-print("Millimeter "+str(mm)+"is converted into "+str(m)+" meters")
-print("Millimeter "+str(mm)+"is converted into "+str(km)+" kilometers")
