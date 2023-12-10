f8bcfb3eec5f63ae007492c896c84e9f3270a33c
PTHARRISH <harrishpt@gmail.com>
2023-12-10 21:04:22 +0530

    Python Private Method

---
+++ Notes/oops/6.inheritance/private_method.py
@@ -0,0 +1,20 @@
+# Python Private Method.
+# In Python, we can define the private method by prefixing the single underscore to the method name,
+# and like the other programming languages.
+# we cannot prevent accessing the private method outside the class in python. 
+# Simply we can say the private method is used only internally for the defined class.
+
+
+# What is private method and public method?
+# A private method is the method that should be called only inside the defined class,
+# whereas the Public method is defined within the class, can be called on an instance of the defined class.
+
+# Now let’s define the private method and see how the private method is differed from the public method.
+
+
+# Example
+# In this example we are going to create a base class by defining the public method and private methods 
+# inside the class and after that we will check whether the private method can be accessible by the other class 
+# or outside the class.
+
+
