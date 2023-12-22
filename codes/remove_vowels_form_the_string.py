2ff22b250a7a31304d9e78504bf7f21b8345f95c
PTHARRISH <harrishpt@gmail.com>
2023-12-22 23:24:56 +0530

    programs

---
+++ codes/remove_vowels_form_the_string.py
@@ -0,0 +1,31 @@
+# remove the vowels
+# 1. loop
+def remove_vowels(sentence):
+    vowels=['a','e','i','o','u']
+    result=""
+    for i in sentence:
+        if i.lower() not in vowels:
+            result+=""+i
+    print(result)
+
+sentence=input("enter the sentence: ")
+remove_vowels(sentence)
+
+# 2. list comprehension
+def remove_vowels(sentence):
+    vowels=['a','e','i','o','u']
+    result=[i for i in sentence if i.lower() not in vowels]
+    result="".join(result) # list(result) to string conversion
+    print(result)
+
+sentence=input("enter the sentence: ")
+remove_vowels(sentence)
+
+# 3 using regular expression
+import re
+def remove_vowels(sentence):
+    return re.sub("[AEIOUaeiou]","",sentence)
+
+
+sentence=input("enter the sentence: ")
+print(remove_vowels(sentence))
