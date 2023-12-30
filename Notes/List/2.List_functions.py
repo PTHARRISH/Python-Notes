936bf1da088968bd8b150e4b4cada72ed6d362ab
PTHARRISH <harrishpt@gmail.com>
2023-12-30 22:20:32 +0530

    list functions

---
+++ Notes/List/2.List_functions.py
@@ -0,0 +1,14 @@
+print("------------------------------------------------------------------------------------------")
+print("---------------------------------Lists Function-------------------------------------------")
+print("------------------------------------------------------------------------------------------")
+
+n=[10,20,30,40]
+
+# len(): return the number of elements present in the list.
+# syntax: len(list) 
+print("Length of the list: ",len(n))
+
+
+# sum(): sum up the numbers in the list.
+# syntax: sum(iterable numbers)
+print("Sum of the list",sum(n))
