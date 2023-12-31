81cae350634c45f5f55b9134194f85be019752ef
PTHARRISH <harrishpt@gmail.com>
2023-12-31 16:51:37 +0530

    problem

--- codes/array/balanced_paranthesis.py
+++ codes/array/balanced_paranthesis.py
@@ -10,3 +10,28 @@ def paranthesis(s):
 
 s=input("enter the paranthesis: ")
 print(paranthesis(s))
+
+
+def is_balanced(expression):
+    stack = []
+    opening_symbols = '({['
+    closing_symbols = ')}]'
+
+    for char in expression:
+        if char in opening_symbols:
+            stack.append(char)
+        elif char in closing_symbols:
+            if not stack:
+                return False  # No matching opening symbol
+            last_opening = stack.pop()
+            if opening_symbols.index(last_opening) != closing_symbols.index(char):
+                return False  # Mismatched opening and closing symbols
+
+    return not stack  # If stack is empty, all symbols are balanced
+
+# Example usage:
+expression1 = "({[()]})"
+expression2 = "{[({[)})"
+
+print("Expression 1 is balanced:", is_balanced(expression1))
+print("Expression 2 is balanced:", is_balanced(expression2))
