45b3253cc2a5a2f64767bb78966f2adf83bc4cb2
PTHARRISH <harrishpt@gmail.com>
2024-01-01 22:50:04 +0530

    min and reduce

--- Notes/List/3.List_functions.py
+++ Notes/List/3.List_functions.py
@@ -43,10 +43,17 @@ reversed_list=list(reversed(n)) # without list function it return reversed objec
 print("Reversed element ",reversed_list) # [5, 4, 3, 2, 1]
 
 
+print("---------------------------------Reduce Function------------------------------------------")
 # reduce():
 # The reduce(fun,seq) function is used to apply a particular function passed in its argument to all 
 # of the list elements mentioned in the sequence passed along.
 # This function is defined in “functools” module.
+import functools 
+lis = [1, 3, 5, 6, 2] 
+print("The sum of the list elements is : ", end="") 
+print(functools.reduce(lambda a, b: a+b, lis)) 
+
+
 
 
 
@@ -56,8 +63,33 @@ print("Reversed element ",reversed_list) # [5, 4, 3, 2, 1]
 # cmp(): This function returns 1 if the first list is “greater” than the second list
 
 
-
+print("---------------------------------Reduce Function------------------------------------------")
 # min(): return minimum element of a given list
+# syntax: min(a, b, c, …, key=func)
+# Parameters
+# a, b, c, .. : similar type of data.
+# key (optional): A function to customize the sort order
+
+# Example 1:
+numbers = [23,25,65,21,98]
+print("Print the minimum number in the list: ",min(numbers))
+print("Print the minimum number in two numbers: ",min(15,75))
+
+# Example 2:
+square = {25: 10, 8: 64, 2: 4, 3: 9, -1: 1, -2: 4}
+print("Print the minimum number in dict key:", min(square))    # -2
+key2 = min(square, key = lambda k: square[k]) # key parameter[-6,8,2,3,-1,-2]
+print("Print the minimum number in dict value:", square[key2])    # 1
+
+# Example 3:
+languages = ["C","Python", "C++ Programming", "Java", "JavaScript",'PHP','Kotlin']
+print("Print the minimum string in ascending order: ", min(languages))
+
+# Example 4:
+def func(x):
+    return abs(x - 10)
+print(min([6, 12, 13, -4, 5], key=func))
+
 
 
 # max(): return maximum element of a given list
