76abba1cf7ef5d26b367bac6e59ea70993921fca
PTHARRISH <harrishpt@gmail.com>
2024-01-04 23:50:02 +0530

    output

--- geek_for_geeks/Program Challenges/subarray_with_given_sum.py
+++ geek_for_geeks/Program Challenges/subarray_with_given_sum.py
@@ -16,6 +16,6 @@ def subarray(arr,s):
          return [-1]
     return [b+1,e+1]
 
-arr=[int(i) for i in input("Enter the array: ").split(",")]
-s=int(input("Target: "))
-print(subarray(arr,s))
+arr=[int(i) for i in input("Enter the array: ").split(",")] # 1,3,5,2,8
+s=int(input("Target: ")) # 10
+print(subarray(arr,s)) # [2, 4]
