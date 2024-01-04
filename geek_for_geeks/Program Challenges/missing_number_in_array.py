784f4f4363c93b38a205b4e6e083a761951d1e4b
PTHARRISH <harrishpt@gmail.com>
2024-01-04 23:40:40 +0530

    output

--- geek_for_geeks/Program Challenges/missing_number_in_array.py
+++ geek_for_geeks/Program Challenges/missing_number_in_array.py
@@ -3,8 +3,8 @@ def missing(arr,n):
     sum_of_a=sum(arr)
     return total-sum_of_a
 
-s=int(input("n: "))
-arr=[int(i) for i in input("Enter the array: ").split(",")]
+s=int(input("n: ")) # n=4
+arr=[int(i) for i in input("Enter the array: ").split(",")] # [1,2,4]
 if len(arr)<=s:
-    print(missing(arr,s))
+    print(missing(arr,s)) # 3
         
