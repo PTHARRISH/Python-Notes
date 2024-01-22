4c979e81ff7facfdbd3cbfb7ff08d3e8f45ace72
PTHARRISH <harrishpt@gmail.com>
2024-01-22 22:25:56 +0530

    coding ninjas

---
+++ coding_ninjas/1.Find_area_of_rectangle.py
@@ -0,0 +1,3 @@
+# 1.Find the area of rectangle
+a,b=map(int,input("Enter two numbers: ").split(","))
+print("Area of rectangle:",a*b)
---
+++ coding_ninjas/2.Print_name_and_age.py
@@ -0,0 +1,5 @@
+# 2. Print name and age:
+name=input("enter the name: ")
+age=int(input("Enter the age: "))
+print("My name is "+name+" and my age is "+str(age))
+print("The name of the person is"," ",name," ","and the age is"," ",age,'.',sep = "")
