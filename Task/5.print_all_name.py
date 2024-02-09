30af6cfd9330880958a34bcb662a724d858f862b
PTHARRISH <harrishpt@gmail.com>
2024-02-09 22:54:35 +0530

    4.time based greeting

---
+++ Task/5.print_all_name.py
@@ -0,0 +1,5 @@
+# print all friends name together
+a=[x for x in input().split(",")]
+print(",".join(a)+" We are all together")
+# Unknown,me,A!
+# Unknown,me,A! We are all together
--- Task/task1.py
+++ Task/task1.py
@@ -6,4 +6,13 @@ print(sd)
 my_list=[1,2,3,4,5,6]
 for i in my_list:
     my_list.remove(i)
-print(my_list)
+print(my_list) # [2,4,6]
+
+# Tuple Check
+x=7,8,9
+print(x==7,8,9) # False 8 9
+
+a=[1,3,5]
+b=[1,2,6,1000]
+print(a>b) # True
+
