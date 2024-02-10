9ce50e22d72374656d8f8d1820c98ec2d881a051
PTHARRISH <harrishpt@gmail.com>
2024-02-10 23:40:31 +0530

    output

--- Task/7.sorting_goals.py
+++ Task/7.sorting_goals.py
@@ -1,5 +1,9 @@
 # Sorting Goals In Ascending Order
-goals=[x for x in input().split(",")]
+goals=[x for x in input("Enter your Goals: ").split(",")]
 print(goals)
 goals.sort()
 print(goals)
+
+# Get a job,Buy a bike,PS5   
+# ['Get a job', 'Buy a bike', 'PS5']
+# ['Buy a bike', 'Get a job', 'PS5']
