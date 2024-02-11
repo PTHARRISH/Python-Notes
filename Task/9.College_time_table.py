a50d59c74b77779c8651d6136683235ca0812e55
PTHARRISH <harrishpt@gmail.com>
2024-02-11 23:26:53 +0530

    college time table

--- Task/9.College_time_table.py
+++ Task/9.College_time_table.py
@@ -1,7 +1,10 @@
 # College Time Table
-def time_table(given_time):
+def time_table(current_time):
     time_period={"9.00":"C","10:00":"C++","11:00":"Break","11:15":"Python","12:15":"DSA"}
-    for time_min,periods in time_period.items():
-        hrs,minu=time_min.split(".")
-        print(hrs,periods)
+    curent_hrs,current_min=map(int,current_time.split("."))
+    for start_time,subject in time_period.items():
+        start_hrs,start_min=map(int,start_time.split("."))
+        if start_hrs<curent_hrs or (start_hrs==curent_hrs)and start_min<=current_min:
+            end_hrs=start_hrs+1
+            if start_min==0 else
 time_table("10:00")
