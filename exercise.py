1d8b7dda26719c0762ef40998aab2b5d7baf51bb
Harrish P T <110607649+PTHARRISH@users.noreply.github.com>
2024-02-13 21:55:12 +0530

    print 1 to 10 reverse

--- exercise.py
+++ exercise.py
@@ -37,3 +37,17 @@ for i in range(1, rows):
     for j in range(i, 0, -1):
         print(j, end=' ')
     print("")
+
+
+
+start = 1
+stop = 2
+current_num = stop
+for row in range(2, 6):
+    for col in range(start, stop):
+        current_num -= 1
+        print(current_num, end=' ')
+    print("")
+    start = stop
+    stop += row
+    current_num = stop
