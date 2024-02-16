79bc9a83099b76853d7de84904655d406fe90ea1
PTHARRISH <harrishpt@gmail.com>
2024-02-16 23:38:46 +0530

    Pascal triangle

--- pattern.py
+++ pattern.py
@@ -110,5 +110,33 @@
 #         min1 = i
 # print(min1)
 
-a=3*4
-print(a)
+
+
+print("Pascal triangle")
+# 1 
+# 1 1 
+# 1 2 1 
+# 1 3 3 1 
+# 1 4 6 4 1 
+# 1 5 10 10 5 1 
+# 1 6 15 20 15 6 1 
+def print_pascal_triangle(size):
+    for i in range(0, size):
+        for j in range(0, i + 1):
+            print(decide_number(i, j), end=" ")
+        print()
+
+def decide_number(n, k):
+    num = 1
+    if k > n - k:
+        k = n - k
+    for i in range(0, k):
+        num = num * (n - i)
+        num = num // (i + 1)
+    return num
+
+
+
+# set rows
+rows = 7
+print_pascal_triangle(rows)
