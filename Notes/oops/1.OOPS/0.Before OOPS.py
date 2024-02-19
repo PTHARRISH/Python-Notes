da3269f1a6e4670f43285471dc02f33d3d3bce05
PTHARRISH <harrishpt@gmail.com>
2024-02-19 22:55:09 +0530

    procedural vs functional

---
+++ Notes/oops/1.OOPS/0.Before OOPS.py
@@ -0,0 +1,54 @@
+# There are three types of programming language
+# Procedural Oriented Programming Language
+# Functional Oriented Programming Language
+# Objected-Oriented Programming Language
+
+# Procedural Programming
+
+# Procedural Programming can be defined as a programming model which is derived from structured programming, 
+# based upon the concept of calling procedure. 
+# Procedures, also known as routines, subroutines or functions, 
+# simply consist of a series of computational steps to be carried out. 
+# During a program’s execution, any given procedure might be called at any point, 
+# including by other procedures or itself. 
+
+# Example:BASIC, Pascal and C
+
+# Procedural Oriented Programming	
+# In procedural programming, the program is divided into small parts called functions.
+# Procedural programming follows a top-down approach.
+# There is no access specifier in procedural programming.
+# Adding new data and functions is not easy.
+# Procedural programming does not have any proper way of hiding data so it is less secure.
+# In procedural programming, overloading is not possible.
+# In procedural programming, there is no concept of data hiding and inheritance.
+# In procedural programming, the function is more important than the data.
+# Procedural programming is based on the unreal world.
+# Procedural programming is used for designing medium-sized programs.
+# Procedural programming uses the concept of procedure abstraction.
+# Code reusability absent in procedural programming,
+
+
+
+# Functional Oriented Programming Language
+# A pure function is a function whose output value follows solely from its input values, 
+# without any observable side effects. 
+# In functional programming, a program consists entirely of evaluation of pure functions. 
+# Computation proceeds by nested or composed function calls, without changes to state or mutable data.
+
+# The functional paradigm is popular because it offers several advantages over other programming paradigms. 
+# Functional code is:
+
+# High level: 
+# You’re describing the result you want rather than explicitly specifying the steps required to get there. 
+# Single statements tend to be concise but pack a lot of punch.
+
+# Transparent: 
+# The behavior of a pure function depends only on its inputs and outputs, without intermediary values. 
+# That eliminates the possibility of side effects, which facilitates debugging.
+
+# Parallelizable: 
+# Routines that don’t cause side effects can more easily run in parallel with one another.
+
+
+
