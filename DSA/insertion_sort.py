29b762c9919bb539990da801927e0952495de553
PTHARRISH <harrishpt@gmail.com>
2024-02-21 22:30:36 +0530

    Inserion sort

--- DSA/insertion_sort.py
+++ DSA/insertion_sort.py
@@ -1,3 +1,26 @@
+# Insertion Sort:
+# Insertion sort is a very simple method to sort numbers in an ascending or descending order. 
+# This method follows the incremental method. 
+# It can be compared with the technique how cards are sorted at the time of playing a game.
+# This is an in-place comparison-based sorting algorithm. 
+# Here, a sub-list is maintained which is always sorted. 
+# For example, the lower part of an array is maintained to be sorted. 
+# An element which is to be 'inserted' in this sorted sub-list, has to find its appropriate place 
+# and then it has to be inserted there. Hence the name, insertion sort.
+
+
+# Pseudocode
+# Algorithm: Insertion-Sort(A)
+# for j = 2 to A.length
+#    key = A[j]
+#    i = j – 1
+#    while i > 0 and A[i] > key
+#       A[i + 1] = A[i]
+#       i = i -1
+#    A[i + 1] = key
+
+
+# code:
 def insertionSort(arr):
 	n = len(arr) # Get the length of the array
 	
