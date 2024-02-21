c0f6c816b76deaea14e49172c67c0b0761d95c9c
PTHARRISH <harrishpt@gmail.com>
2024-02-21 22:45:27 +0530

    array basics

---
+++ DSA/Array.py
@@ -0,0 +1,16 @@
+# Array:
+# An array is a type of linear data structure 
+# that is defined as a collection of elements with same or different data types. 
+# They exist in both single dimension and multiple dimensions. 
+# These data structures come into picture when there is a necessity 
+# to store multiple elements of similar nature together(data type) at one place.
+
+# The difference between an array index and a memory address 
+# array index acts like a key value to label the elements in the array. 
+# a memory address is the starting address of free memory available.
+
+# Following are the important terms to understand the concept of Array.
+
+# Element − Each item stored in an array is called an element.
+# Index − Each location of an element in an array has a numerical index, which is used to identify the element.
+
