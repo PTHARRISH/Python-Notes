851140f8c4384bd4868e7dafd8cb522dfb097143
PTHARRISH <harrishpt@gmail.com>
2024-02-22 22:52:47 +0530

    go through single inked list

--- DSA/single_linkedlist.py
+++ DSA/single_linkedlist.py
@@ -17,11 +17,11 @@ class Sll:
                 print(a.data,end=" ")
                 a=a.next
 
-n1=Node(5) 
-sll=Sll()
-sll.head=n1
-n2=Node(10)
-n1.next=n2
+n1=Node(5) # create a data=5 and pass value in Node class 
+sll=Sll() # create a object of sll 
+sll.head=n1 # now assign a n1 object to sll.head variable now sll.head=5
+n2=Node(10) # and create another data=10 node and pass value in Node class 
+n1.next=n2 # now connect the two nodes
 n3=Node(15)
 n2.next=n3
 n4=Node(20)
