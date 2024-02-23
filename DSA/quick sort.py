e0c99fa56025c125d532a3f3309e1abb15b717af
PTHARRISH <harrishpt@gmail.com>
2024-02-23 22:39:53 +0530

    Quick Sort

---
+++ DSA/quick sort.py
@@ -0,0 +1,27 @@
+# Quick Sort 
+# Approach 1:
+# def partition(data,l,r):
+#     pivot=data[r]
+#     i=l-1#0
+#     for j in range(l,r):
+#         if data[j]<=pivot:
+#             i=i+1
+#             (data[i],data[j])=(data[j],data[i])
+#     (data[i+1],data[r])=(data[r],data[i+1])
+#     return i+1
+
+# def quickSort(data,l,r):
+#     if l<r:
+#         pi=partition(data,l,r)
+#         quickSort(data,l,pi-1)
+#         quickSort(data,pi+1,r)
+
+# data=[1,7,4,1,10,9,-2]
+# print("Unsorted Array")
+# print(data) 
+# r=len(data)-1
+# print(r)
+# quickSort(data,0,r)
+# print('Sorted Array in Ascending Order:')
+# print(data)
+
