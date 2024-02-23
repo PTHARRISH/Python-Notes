6035496c8f9dd190eecd49a0733cc2191fc7ea08
PTHARRISH <harrishpt@gmail.com>
2024-02-23 22:44:29 +0530

    array

--- DSA/Array.py
+++ DSA/Array.py
@@ -14,3 +14,25 @@
 # Element − Each item stored in an array is called an element.
 # Index − Each location of an element in an array has a numerical index, which is used to identify the element.
 
+# An array is a collection of items stored at contiguous memory locations.
+# The idea is to store multiple items of the same type together. 
+# This makes it easier to calculate the position of each element by simply adding an offset to a base value, 
+# i.e., the memory location of the first element of the array (generally denoted by the name of the array).
+
+# Creating an Array
+# Array in Python can be created by importing an array module. 
+# array(data_type, value_list) is used to create an array with data type and value list specified in its arguments. 
+
+# There are thre ways to import array module:
+# Using import (import array)
+# Using import and call alias name (import array as arr)
+# from modules import all methods ( from array import * )
+
+# Some of the data types are mentioned below which will help in creating an array of different data types. 
+# i - signed Int - allow positive and negative values
+# I - unsigned Int - allow only positive values
+# d - double - allow double values
+# f - float - allow floating point values
+# u - unicode character - allow unicode character values
+# b - signed byte integer - allow positive and negative byte integer values
+# B - unsigned byte integer - allow positive byte integer values only
