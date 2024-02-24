8adca59b522ceb7cd07599962677261621d13f95
Harrish P T <110607649+PTHARRISH@users.noreply.github.com>
2024-02-24 23:27:32 +0530

    removing duplicate single linkedlist

--- search.py
+++ search.py
@@ -1,32 +1,3 @@
-# class Node:
-#     def __init__(self,data):
-#         self.data=data
-#         self.next=None
-
-# class Sll:
-#     def __init__(self):
-#         self.head=None
-    
-#     def tranversal(self):
-#         if self.head is None:
-#             print("linked list is empty")
-#         else:
-#             a=self.head
-#             while a is not None:
-#                 print(a.data,end=" ")
-#                 a=a.next
-
-# n1=Node(5)
-# sll=Sll()
-# sll.head=n1
-# n2=Node(10)
-# n1.next=n2
-# n3=Node(15)
-# n2.next=n3
-# n4=Node(20)
-# n3.next=n4
-# sll.tranversal()
-
 # def search(arr,v):
 #     l=len(arr)
 #     for i in range(l):
