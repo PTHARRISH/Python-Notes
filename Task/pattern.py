88f24698e2c42a6d75e172b5af979dbace8d9c65
Harrish P T <110607649+PTHARRISH@users.noreply.github.com>
2024-02-27 22:35:17 +0530

    Update pattern.py

--- Task/pattern.py
+++ Task/pattern.py
@@ -354,3 +354,17 @@ for i in range(0, n):
     for j in range(0, n - i):
         print(i, end=" ")
     print()
+
+
+
+print("--------------------------------------------------------")
+
+#1. Some Generative Art
+# import sys
+# import random
+# chars="\|/"
+# def draw(rows, columns):
+#     for r in rows:
+#         print(''.join(random.choice(chars) for _ in range(columns)))
+
+# draw(10,20)
