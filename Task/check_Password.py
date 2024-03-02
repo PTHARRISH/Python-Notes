06fc3e30d1a74831acaa1c7d4c32d7c242b815a7
Harrish P T <110607649+PTHARRISH@users.noreply.github.com>
2024-03-02 22:33:30 +0530

    password checker Task

---
+++ Task/check_Password.py
@@ -0,0 +1,13 @@
+# 4 to get pwd
+# check alpha num,
+# char more 8 and less than 20
+# pwd = input("Enter your password")# 3483803
+#
+# if(pwd.isalnum() and not pwd.isalpha() and not pwd.isdigit()):
+#     if(len(pwd) > 8 and len(pwd) < 20):
+#         print("Your password is okay")
+#     else:
+#         print("Sorry, your password is not correct, you have to enter pwd, that should be more than 8 and less than 20 char")
+# else:
+#     print("Sorry, include alpha and numaric in your password")
+#
