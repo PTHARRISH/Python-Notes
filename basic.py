8430c65221e0fe926c9471460d6d9b137dce1850
PTHARRISH <harrishpt@gmail.com>
2024-03-08 23:29:57 +0530

    basic

--- basic.py
+++ basic.py
@@ -8,30 +8,6 @@
 # c = input('enter thr string: ')
 # print("The ASCII value of '" + c + "' is", ord(c))
 
-# Python program to print all prime number in an interval calculation
-# def prime(x, y):
-#     prime_list = []
-#     for i in range(x, y):
-#         if i == 0 or i == 1:
-#             continue
-#         else:
-#             for j in range(2, int(i / 2) + 1):
-#                 if i % j == 0:
-#                     break
-#             else:
-#                 prime_list.append(i)
-#     return prime_list
-#
-#
-# starting_range = int(input('Enter the starting range: '))
-# ending_range = int(input('Enter the ending range: '))
-# lst = prime(starting_range, ending_range)
-# if len(lst) == 0:
-#     print("There are no prime numbers in this range")
-# else:
-#     print("The prime numbers in this range are: ", lst)
-
-
 # find the largest number in the list
 # a = [3, 8, 1, 7, 9, 5, 4]
 # b = 0
