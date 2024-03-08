ab51103e63efa027822141255ff95dc04bef68ab
PTHARRISH <harrishpt@gmail.com>
2024-03-08 23:29:20 +0530

    stack defintion

--- DSA/stack.py
+++ DSA/stack.py
@@ -1,3 +1,16 @@
+# Stack
+# Stack is an ordered collection of items where the addition of new items and removal items
+# always taken place at same end.
+# So the end is called as top.
+# And the opposite end is known as base.
+# Stack stored items in Last In First Out (LIFO) or First In Last Out (FILO)
+# Stack stores the data element in similiar fashion ,
+# as the bunch of plates are kept one above the another in a kitchen.
+
+# If you add an object in a stack it will store in a base and second store top of the 1'st object
+# like that way it will stores 
+# If you want to remove the top first object it is called (LIFO)or(FILO)
+
 # Stack implementation in python
 
 print("_______________________________________________________")
@@ -33,3 +46,5 @@ push(stack, str(3))
 push(stack, str(4))
 print("popped item: " + pop(stack))
 print("stack after popping an element: " + str(stack))
+
+
