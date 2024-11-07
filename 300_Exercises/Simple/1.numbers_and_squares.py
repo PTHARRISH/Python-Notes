c94c18293cb7fb3420b832b10bc5aef28a5a5a83
PTHARRISH <harrishpt@gmail.com>
2024-11-07 22:49:03 +0530

    Assignment 2: Calculate the Area of the Rectangle

--- 300_Exercises/Simple/1.numbers_and_squares.py
+++ 300_Exercises/Simple/1.numbers_and_squares.py
@@ -6,6 +6,12 @@ print('2 \t\t '+str(2*2))
 print('3 \t\t '+str(3*3))
 print('4 \t\t '+str(4*4))
 
+# output:
+# Numbers         Squares
+# 1                1
+# 2                4
+# 3                9
+# 4                16
 
 # method 2:
 def number_squares(start_num,end_num):
@@ -16,12 +22,27 @@ def number_squares(start_num,end_num):
         print(f"{n}\t\t{n*n}")
         
 try:
-    start_num,end_num=map(int,input('Enter the starring and ending range with single space:').split(' '))
+    start_num,end_num=map(int,input('Enter the starting and ending range with single space:').split(' '))
     print('Numbers\t\tSquares')
     number_squares(start_num,end_num)
 except ValueError:
     print("Invalid input! Please enter two integers separated by a space.")
 
+
+# output:
+# Enter the starting and ending range with single space:10 20
+# Numbers         Squares
+# 10              100
+# 11              121
+# 12              144
+# 13              169
+# 14              196
+# 15              225
+# 16              256
+# 17              289
+# 18              324
+# 19              361
+# 20              400
 
 # Assignment 1:
 # In Python to find the range of numbers that are both squares and cubes within a specified limit 
@@ -33,12 +54,27 @@ def number_square_cubes(start_num,end_num):
         print(f"{n}\t\t{n*n}\t\t{n*n*n}")
 
 try:
-    start_num,end_num=map(int,input('Enter the starring and ending range with single space:').split(' '))
+    start_num,end_num=map(int,input('Enter the starting and ending range with single space:').split(' '))
     print('Numbers\t\tSquares\t\tCubes')
     number_square_cubes(start_num,end_num)
 except ValueError:
     print("Invalid input! Please enter two integers separated by a space.")
 
+
+# output:
+# Enter the starring and ending range with single space:5 15
+# Numbers         Squares         Cubes
+# 5               25              125
+# 6               36              216
+# 7               49              343
+# 8               64              512
+# 9               81              729
+# 10              100             1000
+# 11              121             1331
+# 12              144             1728
+# 13              169             2197
+# 14              196             2744
+# 15              225             3375
 
 # Time Complexity
 
--- 300_Exercises/Simple/2.area_of_circle.py
+++ 300_Exercises/Simple/2.area_of_circle.py
@@ -3,9 +3,12 @@
 def area_of_circle(n):
     print(f'Area of the circle {n} is {3.14*n*n}')
 
-n = float(input('Enter the n Number: '))
+n = float(input('Enter the area of circle N value : '))
 area_of_circle(n)
 
+# output:
+# Enter the area of circle N value : 10
+# Area of the circle 10.0 is 314.0
 
 # method 2:
 import math
@@ -13,5 +16,28 @@ import math
 def area_of_circe(n):
     print(f'Area of the circle {n} is {math.pi*n**2}')
 
-n = float(input('Enter the n Number: '))
+n = float(input('Enter the area of circle N value : '))
 area_of_circe(n)
+
+# output:
+# Enter the area of circle N value : 20
+# Area of the circle 20.0 is 1256.6370614359173
+
+# Assignment 2:
+# Calculate the Area of the Rectangle
+def area_of_rectangle(l,b):
+    print(f'Area of rectangle with length {l} and breadth {b} is {l * b}')
+
+l,b = map(float, input('Enter the Length and Breadth of the rectangle with single space: ').split(' '))
+area_of_rectangle(l,b)
+
+# output:
+# Enter the Length and Breadth of the rectangle with single space: 10 20
+# Area of rectangle with length 10.0 and breadth 20.0 is 200.0
+
+# Time Complexity:
+# The input handling (map(float, input().split())) takes constant time to process two values, so it's O(1).
+# The function call and the multiplication inside it also take constant time, so that part is O(1).
+
+# Thus, the overall time complexity of the code is O(1), 
+# meaning the runtime does not depend on the size of the input and remains constant.
