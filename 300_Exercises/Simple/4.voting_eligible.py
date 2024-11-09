4502cb327edaea9137cd5366cf01f4019bc781bc
PTHARRISH <harrishpt@gmail.com>
2024-11-09 22:26:10 +0530

    Assignment 4: Calculate the average mark and check eligible fo admission or not

--- 300_Exercises/Simple/4.voting_eligible.py
+++ 300_Exercises/Simple/4.voting_eligible.py
@@ -16,3 +16,36 @@ except ValueError:
 # Enter the name and age with single whitespace: Harrish 19
 # Hi Harrish!. You are eligible for voting your age is 19
 
+
+# Assignment 4:
+# Calculate the average mark and check eligible fo admission or not
+def admission_mark(name,marks):
+    marks = list(map(int, marks))
+    subject=len(marks)
+    total_mark=sum(marks)
+    avg=total_mark//subject
+    if avg>60:
+        print(f'Hi {name}!. you are eligible for taking admission your score is {avg}')
+    else:
+        print(f'Hi {name}!. Sorry you are not eligible for taking admission still you need {60-avg} marks')
+    
+try:
+    user_data = input('Enter the name after enter two whitespaces for marks single space:').split() 
+    name=user_data[0]
+    marks=user_data[1:]
+    admission_mark(name,marks)
+except ValueError:
+    print('please enter the valid input')
+
+# Output:
+# Enter the name after enter two whitespaces for marks single space:Harrish 100 100 100 0
+# Hi Harrish!. you are eligible for taking admission your score is 75
+
+# Time Complexity:
+#     map(int, marks): O(n)
+#     sum(marks): O(n)
+#     Other operations (len(), division, comparison, and printing): O(1)
+# Thus, the overall time complexity of the function is dominated by the map() and sum() operations, 
+# both of which have O(n) complexity.
+
+# Time Complexity: O(n), where n is the number of marks in the input.
