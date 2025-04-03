5c5b632057504d88766756f55f10d808344303de
PTHARRISH <harrishpt@gmail.com>
2025-04-03 23:31:12 +0530

    single number alernate method

--- leet code/75 leetcode/260. Single Number III.py
+++ leet code/75 leetcode/260. Single Number III.py
@@ -17,7 +17,26 @@ def single_number(nums):
             data.append(i)
         if len(data)==2:
             return data
-            
+
+
+def single_number(nums):            
+        n = len(nums)
+        result = [0, 0]
+        index = 0
+
+        for i in range(n):
+            found = False
+            for j in range(n):
+                if i != j and nums[i] == nums[j]:
+                    found = True
+                    break
+            if not found:
+                result[index] = nums[i]
+                index += 1
+                if index == 2:
+                    break
+
+        return result
 
 nums = list(map(int,input('enter the numbers: ').split(',')))
 print(single_number(nums))
