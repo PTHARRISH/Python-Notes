a5f1abd7748045b181711a22caa963392712ad79
Harrish P T <harrishpt@gmail.com>
2025-06-15 23:11:53 +0530

    Linked list

---
+++ DSA/Linked List/Doubled Linkedlist.py
@@ -0,0 +1,11 @@
+# Doubled linkedlist
+class Node:
+    def __init__(self, data):
+        self.data = data  # create data or node
+        self.next = None  # default next is none
+        self.prev = None  # default prev is none
+
+
+class doubled_linked_list:
+    def __init__(self):
+        pass
