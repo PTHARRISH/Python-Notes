3aa8fa8ee6e1878cb46a0eb3f7c22348c0d19722
PTHARRISH <harrishpt@gmail.com>
2025-07-26 01:48:51 +0530

    3487. Maximum Unique Subarray Sum After Deletion

---
+++ leet code/3487. Maximum Unique Subarray Sum After Deletion.py
@@ -0,0 +1,16 @@
+# 3487. Maximum Unique Subarray Sum After Deletion
+def maxSum(nums):
+    data = set()
+    if len(nums) == 1:
+        return nums[0]
+    for i in nums:
+        if i not in data and i > 0:
+            data.add(i)
+    if i < 0:
+        data.add(max(nums))
+    total_data = sum(data)
+    return total_data
+
+
+arr = list(map(int, input("Enter the array:").split(',')))
+print(maxSum(arr))
