7bb885bb34baf50b2faa9e17028948280c84cf22
PTHARRISH <harrishpt@gmail.com>
2025-07-29 20:48:25 +0530

    3 approches

--- geek_for_geeks/challenge/Secondlargest.py
+++ geek_for_geeks/challenge/Secondlargest.py
@@ -1,3 +1,4 @@
+# Using One Iteration(Best method)
 def getSecondLargest(arr):
     n = len(arr)
     first = second = float("-inf")
@@ -14,3 +15,50 @@ def getSecondLargest(arr):
             return -1
         return second
 
+
+# T.C = O(n)
+# S.C = O(1)
+
+
+# Using Two iteration
+def getSecondLargest(arr):
+    n = len(arr)
+    first = second = float("-inf")
+    if n < 2:
+        return -1
+    for i in range(len(arr)):
+        if arr[i] > first:
+            first = arr[i]
+    for i in range(len(arr)):
+        if arr[i] != first and second < first and arr[i] > second:
+            second = arr[i]
+
+    if second == float("-inf"):
+        return -1
+    return second
+
+
+# T.C = O(2n)
+# S.C = O(1)
+
+
+# Using sorted()
+def getSecondLargest(arr):
+    n = len(arr)
+    data = sorted(arr)
+    first = second = float("-inf")
+    if n < 2:
+        return -1
+    else:
+        first = max(arr)
+    for i in range(len(data) - 1, 0, -1):
+        if data[i] != first:
+            second = data[i]
+            break
+    if second == float("-inf"):
+        return -1
+    return second
+
+
+# T.C = O(n log n)
+# S.C = O(1)
