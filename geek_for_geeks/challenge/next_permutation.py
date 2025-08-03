c23c00f47662cc2ccb894cf65a1cf23875ddb0a7
PTHARRISH <harrishpt@gmail.com>
2025-08-03 21:47:42 +0530

    5.nextPermutation

---
+++ geek_for_geeks/challenge/next_permutation.py
@@ -0,0 +1,26 @@
+def nextPermutation(arr):
+    n = len(arr)
+    pivot = -1
+    for i in range(n - 2, -1, -1):
+        if arr[i] < arr[i + 1]:
+            pivot = i
+            break
+    if pivot == -1:
+        arr.reverse()
+        return
+    for i in range(n - 1, pivot, -1):
+        if arr[pivot] < arr[i]:
+            arr[pivot], arr[i] = arr[i], arr[pivot]
+            break
+    left, right = pivot + 1, n - 1
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    return arr
+
+
+print(nextPermutation([1, 2, 4, 5, 6, 0]))
+
+# T.C = O(n)
+# S.C = O(1)
