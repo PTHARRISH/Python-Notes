887befd397e91263e765d0bd01bf2591d3daad27
PTHARRISH <harrishpt@gmail.com>
2025-08-04 22:00:35 +0530

    Majority Element - More Than n/3

---
+++ geek_for_geeks/challenge/majority_element.py
@@ -0,0 +1,19 @@
+# Majority Element - More Than n/3
+def findMajority(arr):
+    data = {}
+    n = len(arr)
+    t = n // 3
+    majority = []
+    for i in arr:
+        if i in data:
+            data[i] += 1
+        else:
+            data[i] = 1
+    for key in data:
+        if data[key] > t:
+            majority.append(key)
+    return sorted(majority)
+
+
+# T.C = O(n)
+# S.C = O(n)
